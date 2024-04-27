3c0454819eb7dda2595519904ce3ed5052d9809e
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-04-27 10:13:58 +0700

    Fix handle query file
    Fix handle query file

--- Carrot_file_script/Carrot_File.cs
+++ Carrot_file_script/Carrot_File.cs
@@ -37,37 +37,48 @@ namespace Carrot
         private FileBrowser.Filter[] SimpleFileBrowser_filter;
         private ExtensionFilter[] StandaloneFileBrowser_filter;
 
-        public void Open_file(Carrot_File_Query query,FileBrowser.OnSuccess Act_done,FileBrowser.OnCancel Act_cancel)
+        private void Handle_filter(Carrot_File_Query query)
         {
-            if (this.type == Carrot_File_Type.SimpleFileBrowser) { 
+            if (this.type == Carrot_File_Type.SimpleFileBrowser)
+            {
                 this.SimpleFileBrowser_filter = new FileBrowser.Filter[query.list_extension_file.Count];
                 for (int i = 0; i < query.list_extension_file.Count; i++)
                 {
-                    
+
                     string[] extension_file = new string[query.list_extension_file[i].Length];
                     string[] extension_file_handle = new string[extension_file.Length];
-                    
-                    for(int y=0;y < extension_file.Length; y++)
+
+                    for (int y = 0; y < extension_file.Length; y++)
                     {
-                        extension_file_handle[y] = "."+ query.list_extension_file[i][y];
+                        extension_file_handle[y] = "." + query.list_extension_file[i][y];
                     }
                     this.SimpleFileBrowser_filter[i] = new FileBrowser.Filter(query.s_title_file_data[i], extension_file_handle);
                 }
                 FileBrowser.SetFilters(true, this.SimpleFileBrowser_filter);
-                if(query.s_DefaultFilter_extension_file!="") FileBrowser.SetDefaultFilter("."+query.s_DefaultFilter_extension_file);
-                FileBrowser.ShowLoadDialog(Act_done, Act_cancel, FileBrowser.PickMode.Files, false);
+                if (query.s_DefaultFilter_extension_file != "") FileBrowser.SetDefaultFilter("." + query.s_DefaultFilter_extension_file);
             }
             else
             {
-                this.StandaloneFileBrowser_filter = new ExtensionFilter[query.list_extension_file.Count+1];
+                this.StandaloneFileBrowser_filter = new ExtensionFilter[query.list_extension_file.Count + 1];
 
-                for(int i = 0; i < this.StandaloneFileBrowser_filter.Length-1; i++)
+                for (int i = 0; i < this.StandaloneFileBrowser_filter.Length - 1; i++)
                 {
                     string[] extension_file = query.list_extension_file[i];
                     this.StandaloneFileBrowser_filter[i] = new ExtensionFilter(query.s_title_file_data[i], extension_file);
                 }
 
                 this.StandaloneFileBrowser_filter[this.StandaloneFileBrowser_filter.Length - 1] = new ExtensionFilter("All Files", "*");
+            }
+        }
+
+        public void Open_file(Carrot_File_Query query,FileBrowser.OnSuccess Act_done,FileBrowser.OnCancel Act_cancel)
+        {
+            this.Handle_filter(query);
+            if (this.type == Carrot_File_Type.SimpleFileBrowser) { 
+                FileBrowser.ShowLoadDialog(Act_done, Act_cancel, FileBrowser.PickMode.Files, false);
+            }
+            else
+            {
                 StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", this.StandaloneFileBrowser_filter, false, (string[] paths) => {
                     if (paths.Length > 0)
                         Act_done(paths);
@@ -77,9 +88,28 @@ namespace Carrot
             }
         }
 
-        public void Save_file()
+        public void Save_file(Carrot_File_Query query,FileBrowser.OnSuccess Act_done, FileBrowser.OnCancel Act_cancel)
         {
-
+            this.Handle_filter(query);
+            if (this.type == Carrot_File_Type.SimpleFileBrowser)
+            {
+                FileBrowser.ShowSaveDialog(Act_done, Act_cancel, FileBrowser.PickMode.Files, false);
+            }
+            else
+            {
+                StandaloneFileBrowser.SaveFilePanelAsync("Save File", "","Myfile", this.StandaloneFileBrowser_filter,(string path) => {
+                    if (path != "")
+                    {
+                        string[] paths = new string[1];
+                        paths[0] = path;
+                        Act_done(paths);
+                    }
+                    else
+                    {
+                        Act_cancel();
+                    } 
+                });
+            }
         }
     }
 }
